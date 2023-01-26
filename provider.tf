119fbc6169ded4f4c7fd7c8d40ab214f52cb1b9a
avirajith <ajith5august@gmail.com>
2023-01-26 00:01:41 -0600

    Create VM with provider

---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+   # version = "1.3.7"
+    features {  }
+subscription_id = "73e834f0-ad18-4bc0-a105-041883eeb7df"
+client_id = "507c4877-d229-48eb-b657-4532643d2e5f"
+client_secret = "RaV8Q~lDLQsf1rNeILd.V4mIh64H0X0MYG8-YcQc"
+tenant_id = "c8db9708-98f7-4eb5-8e0e-62c710c8f34b"
+
+}
